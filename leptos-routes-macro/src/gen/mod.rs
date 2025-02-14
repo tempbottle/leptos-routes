0390efb8d6b1c0ebb7c53eced437fd60ded0dc5c
Lukas Potthast <privat@lukas-potthast.de>
2025-02-14 17:26:19 +0100

    Simplify module insertions; Drop itertools dep

--- leptos-routes-macro/src/gen/mod.rs
+++ leptos-routes-macro/src/gen/mod.rs
@@ -3,7 +3,6 @@ use crate::gen::route_struct::generate_route_struct;
 use crate::gen::router::generate_routes_component;
 use crate::route_def::{flatten, RouteDef};
 use crate::RoutesMacroArgs;
-use itertools::Itertools;
 use proc_macro_error2::abort_call_site;
 use quote::quote;
 use syn::{Item, ItemMod};
@@ -17,21 +16,16 @@ pub fn gen_impls(root_mod: &mut ItemMod, args: RoutesMacroArgs, route_defs: Vec<
     for route_def in flatten(&route_defs) {
         let (struct_def, struct_impl) = generate_route_struct(route_def, &route_defs);
 
-        try_insert_into_module(
-            root_mod,
-            route_def.found_in_module_path.without_first(),
-            struct_def,
-        );
-        try_insert_into_module(
-            root_mod,
-            route_def.found_in_module_path.without_first(),
-            struct_impl,
-        );
+        let src_mod = find_src_module(root_mod, route_def.found_in_module_path.without_first())
+            .expect("present");
+
+        insert_into_module(src_mod, struct_def);
+        insert_into_module(src_mod, struct_impl);
     }
 
     // Generate a "Route" enum listing all possible routes.
     let all_routes_enum = generate_route_enum(&route_defs);
-    try_insert_into_module(root_mod, &[], all_routes_enum);
+    insert_into_module(root_mod, all_routes_enum);
 
     // Generate a "Router" implementation.
     let routes_fn = if args.with_views {
@@ -47,46 +41,39 @@ pub fn gen_impls(root_mod: &mut ItemMod, args: RoutesMacroArgs, route_defs: Vec<
             }
         }
     };
-    try_insert_into_module(root_mod, &[], routes_fn);
+    insert_into_module(root_mod, routes_fn);
 }
 
-pub fn try_insert_into_module(
-    module: &mut ItemMod,
+pub fn find_src_module<'a>(
+    module: &'a mut ItemMod,
     path: &[syn::Ident],
-    ts: proc_macro2::TokenStream,
-) {
-    match syn::parse2::<Item>(ts) {
-        Ok(item) => {
-            insert_into_module(module, path, item);
-        }
-        Err(e) => abort_call_site!(e),
-    }
-}
-
-pub fn insert_into_module(module: &mut ItemMod, path: &[syn::Ident], item: Item) {
+) -> Option<&'a mut ItemMod> {
     if path.is_empty() {
-        if let Some((_, items)) = &mut module.content {
-            items.push(item);
-        } else {
-            abort_call_site!("Expected module to have content");
-        }
-        return;
+        return Some(module);
     }
 
     if let Some((_, items)) = &mut module.content {
-        for sub_item in items.iter_mut() {
-            if let Item::Mod(sub_mod) = sub_item {
-                if sub_mod.ident == path[0] {
-                    insert_into_module(sub_mod, &path[1..], item);
-                    return;
+        for item in items.iter_mut() {
+            if let Item::Mod(child_module) = item {
+                if child_module.ident == path[0] {
+                    return find_src_module(child_module, &path[1..]);
                 }
             }
         }
     }
 
-    abort_call_site!(
-        "Could not find path '{}' in module {}. Could not insert new item {item:?}.",
-        path.iter().map(|it| it.to_string()).join("::"),
-        module.ident.to_string()
-    );
+    None
+}
+
+pub fn insert_into_module(module: &mut ItemMod, ts: proc_macro2::TokenStream) {
+    match syn::parse2::<Item>(ts) {
+        Ok(item) => {
+            if let Some((_, items)) = &mut module.content {
+                items.push(item);
+            } else {
+                abort_call_site!("Expected module to have content");
+            }
+        }
+        Err(e) => abort_call_site!(e),
+    }
 }
